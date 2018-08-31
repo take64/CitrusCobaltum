db32131fb8e3480a356d2169c4926871ee4736cc
kouhei.takemoto <take64@citrus.tk>
2018-09-01 06:23:48 +0900

    Application:実装

---
+++ CitrusCobaltumApplication.h
@@ -0,0 +1,48 @@
+//
+//  CitrusCobaltumApplication.h
+//  CitrusCobaltum
+//
+//  Created by kouhei.takemoto on 2018/09/01.
+//  Copyright © 2018年 citrus.tk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "CitrusTouchTheme.h"
+#import "CitrusTouchCoreData.h"
+
+@interface CitrusCobaltumApplication : NSObject
+{
+//    // theme
+//    CTTheme *theme;
+//
+//    // coredata
+//    CTCoreDataManager *coreDataManager;
+//    NSManagedObjectContext * coreDataContext;
+}
+
+//
+// property
+//
+//@property (nonatomic, retain) CTTheme *theme;
+//@property (nonatomic, retain) CTCoreDataManager *coreDataManager;
+//@property (nonatomic, retain) NSManagedObjectContext * coreDataContext;
+
+
+//
+// static method
+//
+
+// テーマ取得
++ (CTTheme *) callTheme;
+
+//// CoreData manager
+//+ (CTCoreDataManager *) callCoreDataManager;
+//
+//// CoreData context
+//+ (NSManagedObjectContext *) callCoreDataContext;
+
+// singleton
++ (instancetype) sharedApplication;
+
+@end
---
+++ CitrusCobaltumApplication.m
@@ -0,0 +1,107 @@
+//
+//  CitrusCobaltumApplication.m
+//  CitrusCobaltum
+//
+//  Created by kouhei.takemoto on 2018/09/01.
+//  Copyright © 2018年 citrus.tk. All rights reserved.
+//
+
+#import "CitrusCobaltumApplication.h"
+
+#import "CCTheme.h"
+
+@interface CitrusCobaltumApplication()
+
+#pragma mark - property
+//
+// property
+//
+@property CCTheme *theme;
+
+@end
+
+
+@implementation CitrusCobaltumApplication
+
+//
+// synthesize
+//
+//@synthesize theme;
+//@synthesize coreDataManager;
+//@synthesize coreDataContext;
+
+
+
+#pragma mark - method
+//
+// method
+//
+
+// 初期化
+- (instancetype) init
+{
+    self = [super init];
+    if (self)
+    {
+        [self setTheme:[[CTTheme alloc] init]];
+    }
+    return self;
+}
+
+
+
+#pragma mark - static method
+//
+// static method
+//
+
+// テーマ取得
++ (CTTheme *)callTheme
+{
+    CTTheme *theme = [[CitrusCobaltumApplication sharedApplication] theme];
+    if(theme == nil)
+    {
+        theme = [[CTTheme alloc] init];
+    }
+    return theme;
+}
+
+
+//// CoreData manager
+//+ (CTCoreDataManager *)callCoreDataManager
+//{
+//    CTCoreDataManager *_coreDataManager = [[CitrusTouchApplication sharedApplication] coreDataManager];
+//    if(_coreDataManager == nil)
+//    {
+//        CTLog(@"error : CitrusCobaltumApplication.callCoreDataManager");
+//    }
+//    return _coreDataManager;
+//}
+//
+//// CoreData context
+//+ (NSManagedObjectContext *)callCoreDataContext
+//{
+//    NSManagedObjectContext *_coreDataContext = [[CitrusTouchApplication sharedApplication] coreDataContext];
+//    if(_coreDataContext == nil)
+//    {
+//        CTLog(@"error : CitrusCobaltumApplication.callCoreDataContext");
+//    }
+//    return _coreDataContext;
+//}
+
+
+
+#pragma mark - singleton
+//
+// singleton
+//
++ (instancetype) sharedApplication
+{
+    static id singleton = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        singleton = [[CitrusCobaltumApplication alloc] init];
+    });
+    return singleton;
+}
+@end
