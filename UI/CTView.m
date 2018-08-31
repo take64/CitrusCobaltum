b8f08e97c50a2b38df6a94ad9a19220db8bf10da
kouhei.takemoto <take64@citrus.tk>
2018-09-01 01:32:12 +0900

    View:実装

---
+++ UI/CTView.m
@@ -0,0 +1,39 @@
+//
+//  CTView.m
+//  CitrusCobaltum
+//
+//  Created by kouhei.takemoto on 2018/08/27.
+//  Copyright © 2018年 citrus.tk. All rights reserved.
+//
+
+#import "CTView.h"
+
+@implementation CTView
+
+#pragma mark - method
+//
+// method
+//
+
+// 初期化
+- (instancetype)initWithText:(NSString *)textString
+{
+    self = [self initWithFrame:CGRectZero];
+    if(self)
+    {
+        
+    }
+    return self;
+}
+// 初期化
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if(self)
+    {
+        [self setUserInteractionEnabled:NO];
+    }
+    return self;
+}
+
+@end
