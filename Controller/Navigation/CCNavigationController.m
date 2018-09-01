15b1a2fde61278efb222f7fdc91a06d76436a4d2
kouhei.takemoto <take64@citrus.tk>
2018-09-02 00:06:28 +0900

    NavigationController:実装

---
+++ Controller/Navigation/CCNavigationController.m
@@ -0,0 +1,43 @@
+//
+//  CCNavigationController.m
+//  CitrusCobaltum
+//
+//  Created by kouhei.takemoto on 2018/09/01.
+//  Copyright © 2018年 citrus.tk. All rights reserved.
+//
+
+#import "CCNavigationController.h"
+
+#import "CitrusCobaltumApplication.h"
+#import "CCTheme.h"
+
+@interface CCNavigationController ()
+
+@end
+
+@implementation CCNavigationController
+
+#pragma mark - method
+//
+// method
+//
+
+// 初期化
+- (instancetype) initWithRootViewController:(UIViewController *)rootViewController
+{
+    self = [super initWithRootViewController:rootViewController];
+    if (self)
+    {
+        // テーマ
+        CCTheme *theme = [CitrusCobaltumApplication callTheme];
+        
+        // ナヴィゲーション
+        [[self navigationBar] setBarTintColor:[theme callNavigationBarTintColor]];
+        [[self navigationBar] setTitleTextAttributes:@{
+                                                       NSForegroundColorAttributeName   :[theme callNavigationBarTextColor],
+                                                       }];
+    }
+    return self;
+}
+
+@end
