1536d9df16f0df130345b6cc6d0fc42384db2fca
kouhei.takemoto <take64@citrus.tk>
2018-10-23 00:39:55 +0900

    Media:ButtonSound

---
+++ Media/CCMediaSound.h
@@ -0,0 +1,20 @@
+//
+//  CCMediaSound.h
+//  CitrusCobaltum
+//
+//  Created by kouhei.takemoto on 2018/10/23.
+//  Copyright © 2018年 citrus.tk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCMediaSound : NSObject
+
+//
+// method
+//
+
+// ボタン押下音
++ (void) playButtonSound;
+
+@end
---
+++ Media/CCMediaSound.m
@@ -0,0 +1,34 @@
+//
+//  CCMediaSound.m
+//  CitrusCobaltum
+//
+//  Created by kouhei.takemoto on 2018/10/23.
+//  Copyright © 2018年 citrus.tk. All rights reserved.
+//
+
+#import "CCMediaSound.h"
+
+#import <AudioToolbox/AudioToolbox.h>
+
+@implementation CCMediaSound
+
+#pragma mark - method
+//
+// method
+//
+
+// ボタン押下音
++ (void) playButtonSound
+{
+    static SystemSoundID beepSoundId;
+    if(!beepSoundId)
+    {
+        NSURL *beepWavURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"ButtonSound" ofType:@"m4a"] isDirectory:NO];
+        AudioServicesCreateSystemSoundID((__bridge CFURLRef)beepWavURL, &beepSoundId);
+    }
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void) {
+        AudioServicesPlaySystemSound(beepSoundId);
+    });
+}
+
+@end
