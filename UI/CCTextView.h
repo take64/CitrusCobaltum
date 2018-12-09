36377bb3334e254184571852dceaac04885f37c4
kouhei.takemoto <take64@citrus.tk>
2018-12-10 06:00:18 +0900

    CCTextView:実装

---
+++ UI/CCTextView.h
@@ -0,0 +1,13 @@
+//
+//  CCTextView.h
+//  CitrusCobaltum
+//
+//  Created by kouhei.takemoto on 2018/12/10.
+//  Copyright © 2018 citrus.tk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCTextView : UITextView
+
+@end
