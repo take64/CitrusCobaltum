983e0bb32ad67102402658bb49b86541cceccd94
kouhei.takemoto <take64@citrus.tk>
2018-12-12 00:09:24 +0900

    CCImageButton:実装

---
+++ UI/CCImageButton.h
@@ -0,0 +1,20 @@
+//
+//  CCImageButton.h
+//  CitrusCobaltum
+//
+//  Created by kouhei.takemoto on 2018/12/11.
+//  Copyright © 2018 citrus.tk. All rights reserved.
+//
+
+#import "CCButton.h"
+
+@interface CCImageButton : CCButton
+
+//
+// method
+//
+
+// 初期化
+- (instancetype) initWithImageView:(UIImageView *)view;
+
+@end
---
+++ UI/CCImageButton.m
@@ -0,0 +1,57 @@
+//
+//  CCImageButton.m
+//  CitrusCobaltum
+//
+//  Created by kouhei.takemoto on 2018/12/11.
+//  Copyright © 2018 citrus.tk. All rights reserved.
+//
+
+#import "CCImageButton.h"
+
+@interface CCImageButton()
+
+#pragma mark - property
+//
+// property
+//
+@property UIImageView *imageView;
+
+@end
+
+
+
+@implementation CCImageButton
+
+#pragma mark - extends
+//
+// extends
+//
+
+// layoutSubviews
+- (void) layoutSubviews
+{
+    [super layoutSubviews];
+    
+    [[self imageView] setCenter:[self center]];
+}
+
+
+
+#pragma mark - method
+//
+// method
+//
+
+// 初期化
+- (instancetype) initWithImageView:(UIImageView *)view
+{
+    self = [super initWithFrame:[view frame]];
+    if(self)
+    {
+        [self setImageView:view];
+        [self addSubview:view];
+    }
+    return self;
+}
+
+@end
