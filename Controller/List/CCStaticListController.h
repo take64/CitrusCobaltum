bfc7759e311ebeceb9d7553b613149dd185eeff7
kouhei.takemoto <take64@citrus.tk>
2018-12-12 01:09:05 +0900

    CCStaticListController:実装

---
+++ Controller/List/CCStaticListController.h
@@ -0,0 +1,13 @@
+//
+//  CCStaticListController.h
+//  CitrusCobaltum
+//
+//  Created by kouhei.takemoto on 2018/12/12.
+//  Copyright © 2018 citrus.tk. All rights reserved.
+//
+
+#import "CCBaseListController.h"
+
+@interface CCStaticListController : CCBaseListController
+
+@end
